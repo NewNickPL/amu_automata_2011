1496a923e8470c9a14b1712a35957b97e00ed8c4
unknown <NewNickPL@NewNickPL-PC.(none)>
2011-11-24 16:36:50 +0100

    dodanie metody differenceAutomaton

--- daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/AutomataOperations.java
@@ -268,4 +268,28 @@ public class AutomataOperations {
         automaton.addTransition(q0, q2, new EpsilonTransitionLabel());
         return automaton;
     }
+
+    /**
+     * Metoda zwracająca automat akceptujący różnicę języków akceptowanych
+     * przez 2 automaty.
+     */
+    public static AutomatonSpecification differenceAutomaton(
+        AutomatonSpecification automatonA, AutomatonSpecification automatonB) {
+        AutomatonSpecification automatonC = new NaiveAutomatonSpecification();
+        State q0 = automatonC.addState();
+        State q1 = automatonC.addState();
+        State q2 = automatonC.addState();
+        State q3 = automatonC.addState();
+        automatonC.markAsInitial(q0);
+        automatonC.markAsFinal(q3);
+        automatonC.insert(q1, automatonA);
+        //automatonC.insert(q2, reverseLanguageAutomat(
+                //(NaiveAutomatonSpecification) automatonB));
+        //metoda reverseLanguageAutomat nie jest statyczna,
+        //a takze w parametrze przyjmuje NaiveAutomaton, a nie zwykły Automaton.
+        automatonC.addTransition(q0, q1, new EpsilonTransitionLabel());
+        automatonC.addTransition(q1, q2, new EpsilonTransitionLabel());
+        automatonC.addTransition(q2, q3, new EpsilonTransitionLabel());
+        return automatonC;
+    }
 };
