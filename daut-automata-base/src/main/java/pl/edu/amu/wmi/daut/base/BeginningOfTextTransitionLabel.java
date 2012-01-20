6cf645c500c853d70a16c7fde4d8009655d56cbf
unknown <NewNickPL@NewNickPL-PC.(none)>
2012-01-20 13:28:58 +0100

    pierwsza wersja

---
+++ daut-automata-base/src/main/java/pl/edu/amu/wmi/daut/base/BeginningOfTextTransitionLabel.java
@@ -0,0 +1,33 @@
+package pl.edu.amu.wmi.daut.base;
+
+/**
+ * Klasa odpowiedzialna za epsilon-przejscie, gdy automat 
+ * znajduje się na początku napisu
+ */
+public class BeginningOfTextTransitionLabel extends ZeroLengthConditionalTransitionLabel {
+
+    @Override
+    protected boolean doCheckContext(String s, int position) {
+        if ((position < 0) || (position > s.length())) {
+            throw new PositionOutOfStringBordersException();
+        } else if (position == 0) {
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public boolean canAcceptCharacter(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "BeginningOfText";
+    }
+
+}
